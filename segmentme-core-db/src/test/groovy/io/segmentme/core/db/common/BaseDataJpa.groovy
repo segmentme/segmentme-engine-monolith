27666142b4be0f4896131c5c663ee3f077896c34
Artem Slednev <artsiom.slednev@gmail.com>
2020-09-08 17:53:00 +0300

    added spock test

---
+++ segmentme-core-db/src/test/groovy/io/segmentme/core/db/common/BaseDataJpa.groovy
@@ -0,0 +1,21 @@
+package io.segmentme.core.db.common
+
+import groovy.util.logging.Slf4j
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import spock.lang.Specification
+
+@Slf4j
+@DataMongoTest
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+abstract class BaseDataJpa extends Specification {
+
+    @Autowired
+    protected MongoTemplate mongoTemplate
+
+}
