b2ad0a489d877922b0a7e12895d528f9fad94277
Volad <vak.kondratenko@gmail.com>
2020-09-08 12:29:58 +0300

    initial commit

---
+++ segmentme-core-db/src/main/java/io/segmentme/core/db/SpringCoreDataApplication.java
@@ -0,0 +1,13 @@
+package io.segmentme.core.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreDataApplication.class, args);
+	}
+
+}
---
+++ segmentme-core-db/src/test/java/io/segmentme/core/db/SpringCoreDataApplicationTests.java
@@ -0,0 +1,13 @@
+package io.segmentme.core.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
