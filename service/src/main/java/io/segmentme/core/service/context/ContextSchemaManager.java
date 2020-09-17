5e2599aa29eaa4d18a36407ccd7dcfd3035fb470
Volad <vak.kondratenko@gmail.com>
2020-09-17 16:58:21 +0300

    rename service

--- service/src/main/java/io/segmentme/core/service/context/ContextSchemaManager.java
+++ service/src/main/java/io/segmentme/core/service/context/ContextSchemaManager.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class ContextSchemaManagerService {
+public class ContextSchemaManager {
 
     private final ContextSchemaValidationService validationService;
 
