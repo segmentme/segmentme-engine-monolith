a620a7090b6c04fa00262e2b3777431f964c07e2
Volad <vak.kondratenko@gmail.com>
2020-09-22 16:06:14 +0300

    fixed tests

--- database/src/test/groovy/io/segmentme/core/db/SpringCoreDataApplication.java
+++ database/src/test/groovy/io/segmentme/core/db/SpringCoreDataApplication.java
@@ -1,13 +1,11 @@
 package io.segmentme.core.db;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringCoreDataApplication {
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
-    }
+//    @Bean
+//    public ObjectMapper objectMapper() {
+//        return new ObjectMapper();
+//    }
 }
