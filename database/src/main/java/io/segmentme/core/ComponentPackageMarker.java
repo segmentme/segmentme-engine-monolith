5bf2810ccea19733a6bad161133668c9117403c8
Volad <vak.kondratenko@gmail.com>
2020-09-23 15:05:41 +0300

    changes in autoconfiguration

--- database/src/main/java/io/segmentme/core/ComponentPackageMarker.java
+++ database/src/main/java/io/segmentme/core/ComponentPackageMarker.java
@@ -2,9 +2,7 @@ package io.segmentme.core;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-@EnableMongoRepositories(basePackageClasses = ComponentPackageMarker.class)
 public final class ComponentPackageMarker {
 }
