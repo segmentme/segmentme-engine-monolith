3b1548d86190f617b10303c4067acf29e84da31d
Artem Slednev <artsiom.slednev@gmail.com>
2020-09-24 18:05:56 +0300

    rule api tests

--- api/src/test/groovy/io/segmentme/core/api/resource/AnalysisRuleControllerTest.groovy
+++ api/src/test/groovy/io/segmentme/core/api/resource/AnalysisRuleControllerTest.groovy
@@ -9,6 +9,7 @@ import org.springframework.http.MediaType
 
 import static io.segmentme.core.db.domain.condition.AbstractCondition.ConditionType.IN
 import static io.segmentme.core.db.domain.rule.AbstractAnalysisRule.RuleType.BOOLEAN
+import static io.segmentme.core.db.domain.rule.AbstractAnalysisRule.RuleType.PRECONDITION
 import static io.segmentme.core.service.helper.ConditionHelper.fillCondition
 import static io.segmentme.core.service.rule.RuleManagerTest.createRule
 import static java.util.UUID.randomUUID
@@ -26,8 +27,7 @@ class AnalysisRuleControllerTest extends BaseControllerTest {
     @Autowired
     private AbstractAnalysisRuleRepository analysisRuleRepository
 
-
-    def "create rule: #rule"() {
+    def "success creation rule: #rule"() {
         given:
         def ruleToSave = createRule(rule)
         def response = mockMvc.perform(post("/rule/${randomUUID().toString()}")
@@ -38,9 +38,40 @@ class AnalysisRuleControllerTest extends BaseControllerTest {
         response.andExpect(status()
                 .isOk())
                 .andDo(print())
-
+                .andExpect(jsonPath('$.id').isNotEmpty())
+                .andExpect(jsonPath('$.ruleType').value(ruleToSave.ruleType.name()))
+                .andExpect(jsonPath('$.aggregation').value(ruleToSave.aggregation.name()))
+                .andExpect(jsonPath('$.embedded').value(ruleToSave.embedded))
+                .andExpect(jsonPath('$.value').value(ruleToSave.value))
+                .andExpect(jsonPath('$.flags', hasSize(ruleToSave.flags.size())))
+                .andExpect(jsonPath('$.conditions', hasSize(ruleToSave.conditions.size())))
         where:
-        rule                                                                                                           | _
-        ['value': true, 'ruleType': BOOLEAN, 'conditions': List.of(fillCondition(new ArrayConditionDto(), [true], IN))]  | _
+        rule                                                                                                            | _
+        ['value': true, 'ruleType': BOOLEAN, 'conditions': List.of(fillCondition(new ArrayConditionDto(), [true], IN))] | _
+
+    }
+
+    def "success creation precondition rule: #rule"() {
+        given:
+        def ruleToSave = createRule(rule)
+        def response = mockMvc.perform(post("/rule/${randomUUID().toString()}")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(serializeToJson(ruleToSave))
+                .accept(MediaType.APPLICATION_JSON))
+        expect:
+        response.andExpect(status()
+                .isOk())
+                .andDo(print())
+                .andExpect(jsonPath('$.id').isNotEmpty())
+                .andExpect(jsonPath('$.ruleType').value(ruleToSave.ruleType.name()))
+                .andExpect(jsonPath('$.aggregation').value(ruleToSave.aggregation.name()))
+                .andExpect(jsonPath('$.embedded').value(ruleToSave.embedded))
+                .andExpect(jsonPath('$.value').value(ruleToSave.value))
+                .andExpect(jsonPath('$.conditions', hasSize(ruleToSave.conditions.size())))
+                .andExpect(jsonPath('$.analysisRules', hasSize(ruleToSave.analysisRules.size())))
+        where:
+        rule                                                                                                                                                    | _
+        ['value'        : true, 'ruleType': PRECONDITION, 'conditions': List.of(fillCondition(new ArrayConditionDto(), [true], IN)),
+         'analysisRules': List.of(createRule(['value': true, 'ruleType': BOOLEAN, 'conditions': List.of(fillCondition(new ArrayConditionDto(), [true], IN))]))] | _
     }
 }
