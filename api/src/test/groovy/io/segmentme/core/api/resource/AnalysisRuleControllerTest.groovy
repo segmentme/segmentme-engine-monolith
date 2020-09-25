9b155887a2357726b41ee53feea873d24b6b081d
Artem Slednev <artsiom.slednev@gmail.com>
2020-09-25 13:17:04 +0300

    rule api tests

--- api/src/test/groovy/io/segmentme/core/api/resource/AnalysisRuleControllerTest.groovy
+++ api/src/test/groovy/io/segmentme/core/api/resource/AnalysisRuleControllerTest.groovy
@@ -2,6 +2,7 @@ package io.segmentme.core.api.resource
 
 import io.segmentme.core.api.common.BaseControllerTest
 import io.segmentme.core.db.repository.AbstractAnalysisRuleRepository
+import io.segmentme.core.db.repository.AbstractConditionRepository
 import io.segmentme.core.service.dto.component.ArrayConditionDto
 import io.segmentme.core.service.rule.RuleManager
 import org.springframework.beans.factory.annotation.Autowired
@@ -14,6 +15,7 @@ import static io.segmentme.core.service.helper.ConditionHelper.fillCondition
 import static io.segmentme.core.service.rule.RuleManagerTest.createRule
 import static java.util.UUID.randomUUID
 import static org.hamcrest.Matchers.hasSize
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
@@ -26,6 +28,15 @@ class AnalysisRuleControllerTest extends BaseControllerTest {
 
     @Autowired
     private AbstractAnalysisRuleRepository analysisRuleRepository
+
+    @Autowired
+    private AbstractConditionRepository abstractConditionRepository
+
+    def cleanup() {
+        abstractConditionRepository.deleteAll()
+        analysisRuleRepository.deleteAll()
+    }
+
 
     def "success creation rule: #rule"() {
         given:
@@ -73,5 +84,40 @@ class AnalysisRuleControllerTest extends BaseControllerTest {
         rule                                                                                                                                                    | _
         ['value'        : true, 'ruleType': PRECONDITION, 'conditions': List.of(fillCondition(new ArrayConditionDto(), [true], IN)),
          'analysisRules': List.of(createRule(['value': true, 'ruleType': BOOLEAN, 'conditions': List.of(fillCondition(new ArrayConditionDto(), [true], IN))]))] | _
+    }
+
+    def "creation rule validation error: #rule"() {
+        given:
+        def ruleToSave = createRule(rule)
+        def response = mockMvc.perform(post("/rule/${randomUUID().toString()}")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(serializeToJson(ruleToSave))
+                .accept(MediaType.APPLICATION_JSON))
+        expect:
+        response.andExpect(status().isBadRequest()).andExpect(jsonPath('$.errorType').value("VALIDATION_ERROR"))
+        where:
+        rule                                                                                                                           | _
+        ['value': true, 'ruleType': PRECONDITION, 'conditions': List.of(fillCondition(new ArrayConditionDto(), [true], IN))]           | _
+        ['value': true, 'flags': [], 'ruleType': BOOLEAN, 'conditions': List.of(fillCondition(new ArrayConditionDto(), ['type': IN]))] | _
+    }
+
+
+    def "delete rules with embedded rules"() {
+        given:
+        def rules = ['value'        : true, 'ruleType': PRECONDITION, 'conditions': List.of(fillCondition(new ArrayConditionDto(), [true], IN)),
+                     'analysisRules': List.of(
+                             createRule(['value': true, 'embedded': true, 'ruleType': BOOLEAN, 'conditions': List.of(fillCondition(new ArrayConditionDto(), [true], IN, true))]),
+                             createRule(['value': true, 'embedded': false, 'ruleType': BOOLEAN, 'conditions': List.of(fillCondition(new ArrayConditionDto(), [true], IN))])
+                     )]
+        def savedRule = ruleManager.save(createRule(rules), randomUUID().toString(), randomUUID().toString())
+        mockMvc.perform(delete("/rule/${savedRule.id}")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+        when:
+        def existedRules = analysisRuleRepository.findAll()
+        def existedCondition = abstractConditionRepository.findAll()
+        then:
+        existedRules.size() == 1
+        existedCondition.size() == 2
     }
 }
