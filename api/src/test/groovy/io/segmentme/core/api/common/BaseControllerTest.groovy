ad462bdadeae1b6d54e4c2ecf79a407365ccd821
Artem Slednev <ArtemSlednev@coherentsolutions.com>
2020-10-22 10:56:54 +0300

    segment tests

--- api/src/test/groovy/io/segmentme/core/api/common/BaseControllerTest.groovy
+++ api/src/test/groovy/io/segmentme/core/api/common/BaseControllerTest.groovy
@@ -9,10 +9,12 @@ import lombok.SneakyThrows
 import org.spockframework.spring.SpringBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.http.MediaType
 import org.springframework.security.authentication.AbstractAuthenticationToken
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.TestingAuthenticationToken
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultActions
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
 
 @AutoConfigureMockMvc
@@ -54,5 +56,10 @@ class BaseControllerTest extends BaseTestWithContext {
                 .filter(it -> it.getName().equalsIgnoreCase(segmentName))
                 .findFirst()
                 .get()
+    }
+
+    protected ResultActions sendRequest(MockHttpServletRequestBuilder request, Object body = null){
+        def preparedRequest = auth(request).contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
+        return body != null ? mockMvc.perform(preparedRequest.content(serializeToJson(body))) : mockMvc.perform(preparedRequest)
     }
 }
--- api/src/test/groovy/io/segmentme/core/api/resource/StateControllerTest.groovy
+++ api/src/test/groovy/io/segmentme/core/api/resource/StateControllerTest.groovy
@@ -2,17 +2,20 @@ package io.segmentme.core.api.resource
 
 import com.fasterxml.jackson.databind.JsonNode
 import io.segmentme.core.api.common.BaseControllerTest
+import io.segmentme.core.api.error.dto.ErrorType
 import io.segmentme.core.db.repository.StateRepository
+import io.segmentme.core.service.analysis.state.StateManager
 import io.segmentme.core.service.configuration.test.ResourceHolder
 import io.segmentme.core.service.dto.analysis.segment.SegmentDto
 import io.segmentme.core.service.dto.analysis.state.StateDto
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.io.Resource
-import org.springframework.http.MediaType
 
 import static java.util.UUID.randomUUID
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import static org.hamcrest.Matchers.hasItem
+import static org.hamcrest.Matchers.hasSize
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -26,7 +29,10 @@ class StateControllerTest extends BaseControllerTest {
     protected ResourceHolder resourceHolder
 
     @Autowired
-    protected StateRepository stateRepository;
+    protected StateRepository stateRepository
+
+    @Autowired
+    protected StateManager stateManager
 
     def cleanup() {
         stateRepository.deleteAll()
@@ -37,10 +43,7 @@ class StateControllerTest extends BaseControllerTest {
         def stateDto = createState(getSegment(segmentName))
         def workspaceId = randomUUID().toString()
         and:
-        def response = mockMvc.perform(auth(post("/state/workspace/${workspaceId}"))
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(serializeToJson(stateDto))
-                .accept(MediaType.APPLICATION_JSON))
+        def response = sendRequest(post("/state/workspace/${workspaceId}"), stateDto)
         expect:
         response.andExpect(status().isOk())
                 .andDo(print())
@@ -58,7 +61,88 @@ class StateControllerTest extends BaseControllerTest {
     }
 
 
-    StateDto createState(SegmentDto segment) {
+    def 'create state with segment #segmentName - validation fail'() {
+        given:
+        def stateDto = createState(null)
+        stateDto.setName(null).setValue(null)
+        def workspaceId = randomUUID().toString()
+        and:
+        def response = sendRequest(post("/state/workspace/${workspaceId}"), stateDto)
+        expect:
+        response.andExpect(status().isBadRequest())
+                .andDo(print())
+                .andExpect(jsonPath('$.errorType').value(ErrorType.VALIDATION_ERROR.name()))
+                .andExpect(jsonPath('$.errorType').value(ErrorType.VALIDATION_ERROR.name()))
+                .andExpect(jsonPath('$.fieldErrors[*].field', hasItem("segment")))
+                .andExpect(jsonPath('$.fieldErrors[*].field', hasItem("name")))
+        where:
+        segmentName                          | _
+        "SECOND_PHONE_CONTAINS_ONLY"         | _
+        "SECOND_PHONE_CONTAINS_ONLY_SEGMENT" | _
+    }
+
+    def 'update state for workplace'() {
+        given:
+        def state = stateManager.create(randomUUID().toString(), createState(getSegment("SECOND_PHONE_CONTAINS_ONLY_SEGMENT")))
+        state.setName("UPDATED_NAME")
+        when:
+        def response = sendRequest(put("/state/${state.id}"), state)
+        then:
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath('$.id').isNotEmpty())
+                .andExpect(jsonPath('$.name').value("UPDATED_NAME"))
+                .andExpect(jsonPath('$.contextId').value(state.contextId))
+                .andExpect(jsonPath('$.integrationPoint').value(state.integrationPoint))
+                .andExpect(jsonPath('$.segment').isNotEmpty())
+    }
+
+    def 'update state for workplace - validation error'() {
+        given:
+        def state = stateManager.create(randomUUID().toString(), createState(getSegment("SECOND_PHONE_CONTAINS_ONLY_SEGMENT")))
+        state.setName(null)
+        when:
+        def response = sendRequest(put("/state/${state.id}"), state)
+        then:
+        response.andExpect(status().isBadRequest())
+                .andDo(print())
+                .andExpect(jsonPath('$.errorType').value(ErrorType.VALIDATION_ERROR.name()))
+                .andExpect(jsonPath('$.errorType').value(ErrorType.VALIDATION_ERROR.name()))
+                .andExpect(jsonPath('$.fieldErrors[*].field', hasItem("name")))
+    }
+
+    def 'get states for workplace'() {
+        given:
+        def workspaceId = randomUUID().toString()
+        stateManager.create(workspaceId, createState(getSegment("SECOND_PHONE_CONTAINS_ONLY")))
+        stateManager.create(workspaceId, createState(getSegment("SECOND_PHONE_CONTAINS_ONLY_SEGMENT")))
+        when:
+        def response = sendRequest(get("/state/workspace/${workspaceId}"))
+        then:
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath('$[*]', hasSize(2)))
+                .andExpect(jsonPath('$[*].id').isNotEmpty())
+                .andExpect(jsonPath('$[*].name').isNotEmpty())
+                .andExpect(jsonPath('$[*].segment').isNotEmpty())
+    }
+
+    def 'delete state'() {
+        given:
+        def workspaceId = randomUUID().toString()
+        def firstSegment = stateManager.create(workspaceId, createState(getSegment("SECOND_PHONE_CONTAINS_ONLY")))
+        def secondSegment = stateManager.create(workspaceId, createState(getSegment("SECOND_PHONE_CONTAINS_ONLY_SEGMENT")))
+        when:
+        def response = sendRequest(delete("/state/${secondSegment.id}"))
+        then:
+        response.andExpect(status().isOk()).andDo(print())
+        def segments = stateRepository.findAll()
+        segments.size() == 1
+        segments.get(0).id == firstSegment.id
+    }
+
+
+    private StateDto createState(SegmentDto segment) {
         return new StateDto()
                 .setName(randomUUID().toString())
                 .setContextId(randomUUID().toString())
