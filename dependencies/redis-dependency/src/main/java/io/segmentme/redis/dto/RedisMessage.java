884c25e19806f46672064c83fc4e43de066fc3f0
Volad <vak.kondratenko@gmail.com>
2021-01-20 14:22:32 +0300

    rsocket changes

--- dependencies/redis-dependency/src/main/java/io/segmentme/redis/dto/RedisMessage.java
+++ dependencies/redis-dependency/src/main/java/io/segmentme/redis/dto/RedisMessage.java
@@ -1,31 +1,23 @@
 package io.segmentme.redis.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Data;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 @Data
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", visible = true)
 @JsonSubTypes({
-        @JsonSubTypes.Type(value = SegmentStateChangedMessage.class, name = "SEGMENT_STATE_CHANGED"),
+    @JsonSubTypes.Type(value = SegmentStateChangedMessage.class, name = "SEGMENT_STATE_CHANGED"),
 })
 public abstract class RedisMessage implements Serializable {
 
     @JsonIgnore
     public abstract RedisMessageType getType();
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss[.SSS]")
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    private LocalDateTime eventDate = LocalDateTime.now();
+    private Instant eventDate = Instant.now();
 
 }
